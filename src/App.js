c99d0a3223066e3f3eb678c9b1e3e633a958111d
@CaglarTaha <mtaha.mtal@gmail.com>
2022-08-08 20:58:56 +0300

    First Commit
    First Commit About me

---
+++ src/App.js
@@ -0,0 +1,75 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Container'>
+        <div className='Card'>
+          <div className='left'>
+            <h3 className='Tittle'>Question And Answer (Faq)</h3>
+          </div>
+          <div className='right'>
+
+            <div className='card-item-Contaier'>
+            <div className='Row'>
+            <p className='list-item'> Do I have to allow the use of cookies ? </p>
+            <button className='buttonplus'>+</button>
+            </div>
+            </div>
+
+            <div className='card-item-Contaier'>
+            <div className='Row'>
+            <p className='list-item'> Do I have to allow the use of cookies ? </p>
+            <button className='buttonplus'>+</button>
+            </div>
+            </div>
+            <div className='card-item-Contaier'>
+            <div className='Row'>
+            <p className='list-item'> Do I have to allow the use of cookies ? </p>
+            <button className='buttonplus'>+</button>
+            </div>
+            </div>
+            <div className='card-item-Contaier'>
+            <div className='Row'>
+            <p className='list-item'> Do I have to allow the use of cookies ? </p>
+            <button className='buttonplus'>+</button>
+            </div>
+            </div>
+            <div className='card-item-Contaier'>
+            <div className='Row'>
+            <p className='list-item'> Do I have to allow the use of cookies ? </p>
+            <button className='buttonplus'>+</button>
+            </div>
+            </div>
+            <div className='card-item-Contaier'>
+            <div className='Row'>
+            <p className='list-item'> Do I have to allow the use of cookies ? </p>
+            <button className='buttonplus'>+</button>
+            </div>
+            </div>
+            <div className='card-item-Contaier'>
+            <div className='Row'>
+            <p className='list-item'> Do I have to allow the use of cookies ? </p>
+            <button className='buttonplus'>+</button>
+            </div>
+            </div>
+            <div className='card-item-Contaier'>
+            <div className='Row'>
+            <p className='list-item'> Do I have to allow the use of cookies ? </p>
+            <button className='buttonplus'>+</button>
+            </div>
+            </div>
+            
+
+
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
